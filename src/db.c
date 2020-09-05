5dcd242c789935115025b19a632fd0de955ec14c
Youitez <122844222+Youitez@users.noreply.github.com>
2020-09-05 10:15:23 +0000

    ignore ds_store

--- src/db.c
+++ src/db.c
@@ -286,3 +286,6 @@ int load_ts(int id, long long** timestamp, int** open, int** high, int** low, in
     fprintf(stderr, "[ERROR] Failed to allocate memory.\n");
     
     return EXIT_FAILURE;
+  }
+  
+  *open = malloc(records * sizeof(int));
