85124e55af97e8d0455636db0e48c0d986643eda
Youitez <122844222+Youitez@users.noreply.github.com>
2020-09-24 22:18:51 +0000

    [Bundle] Update

--- src/db.c
+++ src/db.c
@@ -313,3 +313,5 @@ int load_ts(int id, long long** timestamp, int** open, int** high, int** low, in
     fprintf(stderr, "[ERROR] Failed to allocate memory.\n");
     
     return EXIT_FAILURE;
+  }
+  
