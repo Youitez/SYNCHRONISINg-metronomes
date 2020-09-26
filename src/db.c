b5026fb2bef46b5582afee24f18b9dda3f89cc2b
Youitez <122844222+Youitez@users.noreply.github.com>
2020-09-26 12:15:11 +0000

    UBI: return ENOSPC if no enough space available

--- src/db.c
+++ src/db.c
@@ -321,3 +321,5 @@ int load_ts(int id, long long** timestamp, int** open, int** high, int** low, in
   {
     fprintf(stderr, "[ERROR] Failed to allocate memory.\n");
     
+    return EXIT_FAILURE;
+  }
