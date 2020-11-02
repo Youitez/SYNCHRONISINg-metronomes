9884f680166ccce0ddf5beaf24d2c751ee3f9716
Youitez <122844222+Youitez@users.noreply.github.com>
2020-11-02 13:38:11 +0000

    chore(release): 1.6.0

--- src/db.c
+++ src/db.c
@@ -419,3 +419,5 @@ int get_row_count_from_slug(char* slug)
         count_string = malloc(PQgetlength(res, i, j) * sizeof(char));
         memcpy((void*)count_string, (void*)PQgetvalue(res, i, j), PQgetlength(res, i, j));
         count_string[PQgetlength(res, i, j)] = '\0';
+      }
+    }
