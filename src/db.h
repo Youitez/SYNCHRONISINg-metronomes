207ec1cdaa5aed2cef08896ab8b9600c4f43df30
Youitez <122844222+Youitez@users.noreply.github.com>
2020-11-09 06:20:40 +0000

    Added translation using Weblate (Chinese (Traditional))

--- src/db.h
+++ src/db.h
@@ -7,3 +7,7 @@ int get_name_from_id(int id, char* name);
 int get_ticker_from_id(int id, char* ticker);
 int get_id_from_ticker(char* ticker);
 int get_row_count_from_slug(char* slug);
+
+int load_ts(int id, long long** timestamp, int** open, int** high, int** low, int** close, int** volume);
+
+#endif
