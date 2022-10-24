54f136dddc12acc827832617caa873576514d22c
Youitez <122844222+Youitez@users.noreply.github.com>
2022-10-24 18:42:35 +0000

    sync(vim)

--- src/main.c
+++ src/main.c
@@ -63,4 +63,4 @@ int main(int argc, char** argv)
     printf("Security is currently trading $%.2f below historical averages.\n", 
            (get_mean(close, num_rows)/100) - ((float)(close[0]/100)));
   }
-  else if(close[0] >= get_mea
+  else if(close[0] >= get_mean(c
